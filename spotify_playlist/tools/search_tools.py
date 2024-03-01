ea0009862cda681e7e34b5251fc6937e4c95e450
Ivan Fioravanti <ivan.fioravanti@gmail.com>
2024-03-01 21:09:42 +0100

    Refactor SearchTools class in search_tools module
    The code has been cleaned to improve readability and maintainability in the SearchTools class of the search_tools module. This included adjusting the formatting of dictionary items and their usage in the string append function and removing unnecessary white spaces. Edge cases such as a KeyError exception during result parsing have also been handled.

--- spotify_playlist/tools/search_tools.py
+++ spotify_playlist/tools/search_tools.py
@@ -2,12 +2,13 @@ import requests, os
 import json
 from langchain.tools import tool
 
-class SearchTools():
+
+class SearchTools:
 
     @tool("Search the internet")
     def search_internet(query):
         """Userful to search on internet about a given topic and return relevant results"""
-        
+
         print("Searching the internet...", query)
         top_result_to_return = 4
         url = f"https://google.serper.dev/search"
@@ -15,10 +16,10 @@ class SearchTools():
         payload = json.dumps({"q": query})
         headers = {
             "X-API-KEY": os.environ["SERPER_API_KEY"],
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         }
 
-        response = requests.request("POST", url, headers=headers, data=payload) 
+        response = requests.request("POST", url, headers=headers, data=payload)
         if "organic" not in response.json():
             return "No results found"
         else:
@@ -26,11 +27,20 @@ class SearchTools():
             string = []
             for result in results[:top_result_to_return]:
                 try:
-                    string.append("\n".join([f"Title: {result["title"]}", f"Link: {result["link"]}", f"Snippet: {result["snippet"]}"]))
+                    string.append(
+                        "\n".join(
+                            [
+                                f"Title: {result['title']}",
+                                f"Link: {result['link']}",
+                                f"Snippet: {result['snippet']}",
+                            ]
+                        )
+                    )
                 except KeyError:
                     next
 
             return "\n".join(string)
-        
+
+
 if __name__ == "__main__":
     print(SearchTools.search_internet("How to make a cake"))
